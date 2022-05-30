9a282edbf7493f9d242ab65849b8531535ce359b
Sergey Agafonov <seriywolf@gmail.com>
2022-05-30 16:31:27 +0700

    Add header with logo

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-//const pwd = document.getElementById("password");
-//const pwd2 = document.getElementById("password2");
-
-/*if (pwd.textContent!==""||pwd2.textContent!==""){    
-    pwd.style.borderColor = 'red';
-    pwd2.style.borderColor = 'red';
-};
-*/
 const pwd = document.getElementById("password");
 const pwd2 = document.getElementById("password2");
 
 pwd.onclick = function () {
   if (pwd.style.borderColor === "red") {
-      console.log("!")
-    //pwd.value = "";
     pwd.style.borderColor = "";
     pwd2.style.borderColor = "";
     document.querySelector("span").textContent = "";
@@ -21,7 +11,6 @@ pwd.onclick = function () {
 
 pwd2.onclick = function () {
     if (pwd2.style.borderColor === "red") {
-      //pwd2.value = "";
       pwd.style.borderColor = "";
       pwd2.style.borderColor = "";
       document.querySelector("span").textContent = "";
